d110e5b9924d0fc602e282839309f7d44dc88a5f
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2024-01-16 10:51:24 +0200

    destination page

--- index.js
+++ index.js
@@ -14,3 +14,66 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+//Tabs
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener("keydown", changeTabFocus);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+  if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+    e.preventDefault();
+    tabs[tabFocus].setAttribute("tabindex", -1);
+
+    if (e.key === "ArrowRight") {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    }
+
+    if (e.key === "ArrowLeft") {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e) {
+  const targetTab = e.target;
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  const targetImage = targetTab.getAttribute("data-image");
+
+  const tabContainer = targetTab.parentNode;
+  const mainContainer = tabContainer.parentNode;
+
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
+  mainContainer
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((panel) => panel.setAttribute("hidden", true));
+
+  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+
+  mainContainer
+    .querySelectorAll("picture")
+    .forEach((picture) => picture.setAttribute("hidden", true));
+
+  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+}
